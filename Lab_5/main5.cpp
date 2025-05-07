2ef982a633d9d090e005a1d4d7320f6bfbdda090
Rostyslav Kozachok <131653593+Rost04ek@users.noreply.github.com>
2025-05-07 15:57:08 +0300

    Update main5.cpp

--- Lab_5/main5.cpp
+++ Lab_5/main5.cpp
@@ -6,8 +6,10 @@ int main() {
     SetConsoleCP(65001);
 
     std::cout << "=== Створення об'єкта HybridPlant ===" << std::endl;
-    HybridPlant my_hybrid("Гібридус", "Мандарини", "Фіолетовий");
+    HybridPlant my_hybrid("Гібридус", "Фіолетовий", 350);
     my_hybrid.PrintInfo();
+    my_hybrid.Describe();
     std::cout << "=== Видалення об'єкта HybridPlant ===" << std::endl;
+
     return 0;
 }
