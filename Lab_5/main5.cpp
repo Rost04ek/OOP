ac682283c09d2491e1b583f13494c7e52043ef9c
Rostyslav Kozachok <131653593+Rost04ek@users.noreply.github.com>
2025-05-07 16:30:12 +0300

    Update main5.cpp

--- Lab_5/main5.cpp
+++ Lab_5/main5.cpp
@@ -6,10 +6,18 @@ int main() {
     SetConsoleCP(65001);
 
     std::cout << "=== Створення об'єкта HybridPlant ===" << std::endl;
-    HybridPlant my_hybrid("Гібридус", "Фіолетовий", 350);
-    my_hybrid.PrintInfo();
-    my_hybrid.Describe();
-    std::cout << "=== Видалення об'єкта HybridPlant ===" << std::endl;
+    HybridPlant* my_hybrid = new HybridPlant("Гібридус", "Фіолетовий", 350);
+    my_hybrid->PrintInfo();
+    my_hybrid->Describe();
+    std::cout << "=== Видалення об'єкта Hybridplant ===" << std::endl;
+    delete my_hybrid;
+
+    std::cout << "=== Створення об'єкта MegaPlant ===" << std::endl;
+    MegaPlant* my_mega_plant = new MegaPlant();
+    my_mega_plant->PrintInfo();
+    my_mega_plant->Describe();
+    std::cout << "=== Видалення об'єкта Megaplant ===" << std::endl;
+    delete my_mega_plant;
 
     return 0;
 }
